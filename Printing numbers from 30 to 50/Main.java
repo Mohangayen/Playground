848e2b8bc75cb08dbaf5a5da93dbc408f20d8dae
Mohangayen <46418097+Mohangayen@users.noreply.github.com>
2019-01-19 20:30:51 +0530

    Test code pushed to repository.

---
+++ Printing numbers from 30 to 50/Main.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+class Main {
+	public static void main (String[] args){
+	     // Type your code here
+      int count =1;
+      for (int i=30; i<=50; i++)
+        
+      {
+        System.out.println(i);
+      }
+	}
+}
